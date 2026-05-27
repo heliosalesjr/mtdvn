89f90b2a96835692abefedb8ac344cdb325cb094
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2026-05-27 12:50:20 -0300

    deceleration and set up

--- character.gd
+++ character.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+@export var _move_speed: float = 2560
+@export var _acceleration: float = 512
+@export var _deceleration: float = 2048
+
 var direction: float 
 
 func _physics_process(delta: float) -> void:
@@ -11,11 +16,13 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	#var direction := Input.get_axis("move_left", "move_right")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		if velocity.x == 0 or sign(velocity.x) == sign(direction):
+			velocity.x = move_toward(velocity.x, direction * _move_speed, _acceleration * delta)
+		else:
+			velocity.x = move_toward(velocity.x, direction * _move_speed, _deceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, _deceleration * delta)
 		
 	move_and_slide()
